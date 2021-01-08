82a2c44c6c2075e73acf9e24def0e0aecb6eb408
Michal Duszak <drduszak@gmail.com>
2021-01-08 05:37:55 +0100

    updated reservations controller and routes

--- controllers/reservationsController.js
+++ controllers/reservationsController.js
@@ -1,13 +1,178 @@
-const GuestRepository = require('../repository/sequelize/GuestRepository');
 const ReservationRepository = require('../repository/sequelize/ReservationRepository');
+const TableRepository = require('../repository/sequelize/TableRepository');
+const GuestRepository = require('../repository/sequelize/GuestRepository');
 
-
-exports.showReservationsList = (req, res, next) => {
-    res.render('pages/rezerwacje-list', {navLocation: 'reservations'});
+exports.showReservationList = (req, res, next) => {
+    ReservationRepository.getReservations()
+        .then(reservations => {
+            res.render('pages/rezerwacje-list', {
+                reservations: reservations,
+                navLocation: 'reservations'
+            });
+        });
+}
+exports.showAddReservationForm = (req, res, next) => {
+    let allGuests, allTables;
+    const validationErrors = [];
+    GuestRepository.getGuests()
+        .then(gsts => {
+            allGuests = gsts;
+            return TableRepository.getTables();
+        })
+        .then(tbs => {
+            allTables = tbs;
+            res.render('pages/rezerwacje-list', {
+                reservation: {},
+                reservationData: {},
+                formMode: 'createNew',
+                allGuests: allGuests,
+                allTables: allTables,
+                pageTitle: 'New reservation',
+                btnLabel: 'Add reservation',
+                formAction: '/reservations/add',
+                navLocation: 'reservations',
+                validationErrors: validationErrors
+            });
+        });
 }
 
-exports.showAddReservationsForm = (req, res, next) => {
-    
-    res.render('pages/rezerwacje-form',{navLocation: "reservations"})
-    res.render('pages/table-form', { navLocation: 'employment'});
-        }
+exports.showEditReservationForm = (req, res, next) => {
+    const reservationId = req.params.reservationId;
+    let allGuests, allTables;
+    const validationErrors = [];
+
+    GuestRepository.getGuests()
+        .then(gsts => {
+            allGuests = gsts;
+            return TableRepository.getTables();
+        })
+        .then(tbs => {
+            allTables = tbs;
+            return ReservationRepository.getReservationById(reservationId);
+
+        })
+        .then(reservation => {
+            res.render('pages/rezerwacje-form', {
+                reservation: reservation,
+                reservationData: reservation,
+                formMode: 'edit',
+                allGuests: allGuests,
+                allTables: allTables,
+                pageTitle: 'Edit reservation',
+                btnLabel: 'Edit reservation',
+                formAction: '/reservations/edit',
+                navLocation: 'reservations',
+                validationErrors: validationErrors
+            });
+        });
+}
+
+
+exports.showReservationDetails = (req, res, next) => {
+    const reservationId = req.params.reservationId;
+    let allGuests, allTables;
+    const validationErrors = [];
+
+    GuestRepository.getGuests()
+        .then(gsts => {
+            allGuests = gsts;
+            return TableRepository.getTables();
+        })
+        .then(tbs => {
+            allTables = tbs;
+            return ReservationRepository.getReservationById(reservationId);
+
+        })
+        .then(reservation => {
+            res.render('pages/rezerwacje-form', {
+                reservation: reservation,
+                reservationData: reservation,
+                formMode: 'showDetails',
+                allGuests: allGuests,
+                allTables: allTables,
+                pageTitle: 'Reservation details',
+                formAction: '',
+                navLocation: 'reservations',
+                validationErrors: validationErrors
+            });
+        });
+}
+
+
+exports.addReservation = (req, res, next) => {
+
+    const reservationData = { ...req.body };
+    let allGuests, allTables;
+
+    ReservationRepository.createReservation(reservationData)
+        .then(result => {
+            res.redirect('/reservations');
+        })
+        .catch(err => {
+            GuestRepository.getGuests()
+                .then(gsts => {
+                    allGuests = gsts;
+                    return TableRepository.getTables();
+                })
+                .then(tbs => {
+                    allTables = tbs;
+                    res.render('pages/rezerwacje-form', {
+                        reservation: {},
+                        reservationData: reservationData,
+                        formMode: 'createNew',
+                        allGuests: allGuests,
+                        allTables: allTables,
+                        pageTitle: 'New reservation',
+                        btnLabel: 'Add reservation',
+                        formAction: '/reservations/add',
+                        navLocation: 'reservations',
+                        validationErrors: err.errors
+                    });
+
+                });
+
+        });
+};
+
+exports.updateReservation = (req, res, next) => {
+    const reservationId = req.body._id;
+    const reservationData = { ...req.body };
+    let allGuests, allTables;
+
+    ReservationRepository.updateReservation(reservationId, reservationData)
+        .then(result => {
+            res.redirect('/reservations');
+        })
+        .catch(err => {
+            GuestRepository.getGuests()
+                .then(gsts => {
+                    allGuests = gsts;
+                    return TableRepository.getTables();
+                })
+                .then(tbs => {
+                    allTables = tbs;
+                    return ReservationRepository.getReservationById(reservationId);
+
+                }).then(reservation => {
+                    res.render('pages/rezerwacje-form', {
+                        reservation: reservation,
+                        formMode: 'edit',
+                        allGuests: allGuests,
+                        allTables: allTables,
+                        pageTitle: 'Edit reservation',
+                        btnLabel: 'Edit reservation',
+                        formAction: '/reservations/edit',
+                        navLocation: 'reservations',
+                        validationErrors: err.errors
+                    });
+                });
+        });
+};
+
+exports.deleteReservation = (req, res, next) => {
+    const reservationId = req.params.reservationId;
+    ReservationRepository.deleteReservation(reservationId)
+        .then(() => {
+            res.redirect('/reservations');
+        });
+};
--- routes/reservationsRoute.js
+++ routes/reservationsRoute.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const reservationsController = require('../controllers/reservationsController.js');
-router.get('/', reservationsController.showReservationsList);
-router.get('/add', reservationsController.showAddReservationsForm);
-// router.get('/details/:empId', guestsControler.showGuestsDetails);
+
+const reservationController = require('../controllers/reservationsController');
+
+router.get('/', reservationController.showReservationList);
+router.get('/add', reservationController.showAddReservationForm);
+router.get('/edit/:reservationId', reservationController.showEditReservationForm);
+router.get('/details/:reservationId', reservationController.showReservationDetails);
+
+router.post('/add', reservationController.addReservation);
+router.post('/edit', reservationController.updateReservation);
+router.get('/delete/:reservationId', reservationController.deleteReservation);
+
 module.exports = router;
